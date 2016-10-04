6f0a47bf722d5eedcf76c08403d5ad7237bfead2
Kent Gruber <kgruber1@emich.edu>
2016-10-03 21:51:58 -0400

    Tabs = 2 Spaces
    Because personal preferences.

--- routes/errors.rb
+++ routes/errors.rb
@@ -1,17 +1,17 @@
 module Sinatra
-	module App
-		module Routing
-			module Errors
-				def self.registered(app)
-					app.error do
-						"Y U NO WORK?"					
-					end
+  module App
+    module Routing
+      module Errors
+        def self.registered(app)
+          app.error do
+            "Y U NO WORK?"
+          end
 
-					app.not_found do
-						halt 404, "Whoops! You requested a route that wasn't available."
-					end
-				end
-			end
-		end
-	end
+          app.not_found do
+            halt 404, "Whoops! You requested a route that wasn't available."
+          end
+        end
+      end
+    end
+  end
 end
